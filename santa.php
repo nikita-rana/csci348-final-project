c8c34754d0323df53f070eabe84e255242cebff6
nikita-rana <59716015+nikita-rana@users.noreply.github.com>
2022-11-21 17:38:15 -0800

    Create santa.php
    Main content still needed to be added

---
+++ santa.php
@@ -0,0 +1,43 @@
+<?php
+include head.php
+?>
+
+<body>
+    <div class="wrapper1">
+
+    <header class ="logo"> Christmas Website </header>
+
+    <nav clas ="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">Wishlist</a></li>
+            <li><a href="">Secret Santa</a></li>
+        </ul>
+    </nav>
+
+    <div class="side-nav">
+        <ul>
+            <li class="item1"><a href="signin.php">Sign In</a></li>
+            <li class="item2"><a href="register.php">Sign Up</a></li>
+        </ul>
+
+    </div>
+
+
+    <div class="main-content">
+
+
+    </div>
+
+    <div class = "footer">
+        <?php
+        include 'footer.php';
+        ?>
+    </div>
+
+
+
+
+    </div>
+
+</body>
